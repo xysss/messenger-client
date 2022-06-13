c3fbea7cb9d833058035b9d79fa4d331277f01cb
xuyongsheng <1343025166@qq.com>
2022-06-13 14:56:25 +0800

    add 测试图片缩放功能

--- app/src/main/java/com/example/messengerclientdemo/MessengerActivity.kt
+++ app/src/main/java/com/example/messengerclientdemo/MessengerActivity.kt
@@ -6,11 +6,20 @@ import android.content.Intent
 import android.content.ServiceConnection
 import android.os.*
 import android.util.Log
+import android.view.MotionEvent
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.example.messengerclientdemo.databinding.ActivityMessengerBinding
+import androidx.core.view.MotionEventCompat
+import com.bm.library.PhotoView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.MultiTransformation
+import com.bumptech.glide.request.RequestOptions
 import com.example.model.Person
 import com.example.model.User
 import com.example.model.UserS
+import jp.wasabeef.glide.transformations.BlurTransformation
+import jp.wasabeef.glide.transformations.GrayscaleTransformation
+import jp.wasabeef.glide.transformations.RoundedCornersTransformation
 
 
 /**
@@ -20,13 +29,22 @@ import com.example.model.UserS
  */
 class MessengerActivity : AppCompatActivity(){
 
+    private val url = "https://i.bmp.ovh/imgs/2022/06/09/295d6dc338bc0b0d.png"
+
+    private lateinit var mPhotoView: com.github.chrisbanes.photoview.PhotoView
+    private lateinit var btnConnect: Button
+    private lateinit var btnSayHello: Button
+    private lateinit var mPhotoView2: com.bm.library.PhotoView
+
+
+
     companion object {
         const val WHAT1 = 1
         const val WHAT2 = 2
         const val WHAT3 = 3
     }
 
-    private lateinit var viewBinding: ActivityMessengerBinding
+    //private lateinit var viewBinding: ActivityMessengerBinding
 
     /** 与服务端进行沟通的Messenger */
     private var mService: Messenger? = null
@@ -36,6 +54,8 @@ class MessengerActivity : AppCompatActivity(){
 
     /** 是否已bindService */
     private var bound: Boolean = false
+
+    private val DEBUG_TAG="DEBUG_TAG"
 
     private val mServiceConnection = object : ServiceConnection {
 
@@ -50,18 +70,91 @@ class MessengerActivity : AppCompatActivity(){
         }
     }
 
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+
+        val action: Int = MotionEventCompat.getActionMasked(event)
+
+        return when (action) {
+            MotionEvent.ACTION_DOWN -> {
+                Log.d(DEBUG_TAG, "Action was DOWN")
+                true
+            }
+            MotionEvent.ACTION_MOVE -> {
+                Log.d(DEBUG_TAG, "Action was MOVE")
+                true
+            }
+            MotionEvent.ACTION_UP -> {
+                Log.d(DEBUG_TAG, "Action was UP")
+                true
+            }
+            MotionEvent.ACTION_CANCEL -> {
+                Log.d(DEBUG_TAG, "Action was CANCEL")
+                true
+            }
+            MotionEvent.ACTION_OUTSIDE -> {
+                Log.d(DEBUG_TAG, "Movement occurred outside bounds of current screen element")
+                true
+            }
+            else -> super.onTouchEvent(event)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        viewBinding = ActivityMessengerBinding.inflate(layoutInflater)
-        setContentView(viewBinding.root)
+        setContentView(R.layout.activity_messenger)
 
-        viewBinding.btnConnect.setOnClickListener {
+//        viewBinding = ActivityMessengerBinding.inflate(layoutInflater)
+//        setContentView(viewBinding.root)
+
+        mPhotoView = findViewById(R.id.photo_view)
+        mPhotoView2 = findViewById(R.id.photo_view2)
+
+        btnConnect = findViewById(R.id.btnConnect)
+        btnSayHello = findViewById(R.id.btnSayHello)
+
+
+
+        btnConnect.setOnClickListener {
             connectService()
         }
-        viewBinding.btnSayHello.setOnClickListener {
+        btnSayHello.setOnClickListener {
             sayHello()
         }
+
+
+        //模糊处理 BlurTransformation
+        //灰度处理 GrayscaleTransformation
+        //圆角处理 RoundedCornersTransformation
+        val multi = MultiTransformation(
+            BlurTransformation(1),
+            GrayscaleTransformation(),
+            RoundedCornersTransformation(50, 0, RoundedCornersTransformation.CornerType.ALL)
+        )
+
+        //设置Glide的apply的参数
+        val option= RequestOptions()
+            .transform(multi)
+
+        Glide.with(this)
+            .load(R.drawable.a)
+            .apply(option)
+            .placeholder(R.drawable.ic_launcher_background)
+            .into(mPhotoView)
+
+
+
+        val options2=RequestOptions()
+            //.transform(BlurTransformation(25))
+        // 启用图片缩放功能
+        mPhotoView2.enable()
+        Glide.with(this)
+            .load(R.drawable.a)
+            //.load(url)
+            .apply(options2)
+            .placeholder(R.drawable.ic_launcher_background)
+            .into(mPhotoView2)
+
     }
 
     private fun sayHello() {
